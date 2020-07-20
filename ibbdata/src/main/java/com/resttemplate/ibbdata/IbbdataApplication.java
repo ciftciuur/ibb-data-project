dc4a7dbfd73ddd8297755b924c24c87e433b22a0
ugur ciftci <ugurprog@gmail.com>
2020-07-20 21:21:18 +0300

    created spring boot project

---
+++ ibbdata/src/main/java/com/resttemplate/ibbdata/IbbdataApplication.java
@@ -0,0 +1,13 @@
+package com.resttemplate.ibbdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbbdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbbdataApplication.class, args);
+	}
+
+}
---
+++ ibbdata/src/test/java/com/resttemplate/ibbdata/IbbdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resttemplate.ibbdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbbdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
