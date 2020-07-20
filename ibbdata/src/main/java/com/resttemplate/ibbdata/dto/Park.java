2150bcc3a07b1701848165614e34ca74fde2adbe
ugur ciftci <ugurprog@gmail.com>
2020-07-20 21:53:16 +0300

    added resttemplate bean

--- ibbdata/src/main/java/com/resttemplate/ibbdata/dto/Park.java
+++ ibbdata/src/main/java/com/resttemplate/ibbdata/dto/Park.java
@@ -1,5 +1,8 @@
 package com.resttemplate.ibbdata.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Park {
 
     //Park Id bilgisini içerir.
@@ -24,4 +27,84 @@ public class Park {
     private double Distance;
     //ücretsiz parklanma süresi
     private int UcretsizParklanmaDk;
+
+    public int getParkID() {
+        return ParkID;
+    }
+
+    public void setParkID(int parkID) {
+        ParkID = parkID;
+    }
+
+    public String getParkAdi() {
+        return ParkAdi;
+    }
+
+    public void setParkAdi(String parkAdi) {
+        ParkAdi = parkAdi;
+    }
+
+    public double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        Latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        Longitude = longitude;
+    }
+
+    public int getKapasitesi() {
+        return Kapasitesi;
+    }
+
+    public void setKapasitesi(int kapasitesi) {
+        Kapasitesi = kapasitesi;
+    }
+
+    public int getBosKapasite() {
+        return BosKapasite;
+    }
+
+    public void setBosKapasite(int bosKapasite) {
+        BosKapasite = bosKapasite;
+    }
+
+    public String getParkTipi() {
+        return ParkTipi;
+    }
+
+    public void setParkTipi(String parkTipi) {
+        ParkTipi = parkTipi;
+    }
+
+    public String getIlce() {
+        return Ilce;
+    }
+
+    public void setIlce(String ilce) {
+        Ilce = ilce;
+    }
+
+    public double getDistance() {
+        return Distance;
+    }
+
+    public void setDistance(double distance) {
+        Distance = distance;
+    }
+
+    public int getUcretsizParklanmaDk() {
+        return UcretsizParklanmaDk;
+    }
+
+    public void setUcretsizParklanmaDk(int ucretsizParklanmaDk) {
+        UcretsizParklanmaDk = ucretsizParklanmaDk;
+    }
 }
